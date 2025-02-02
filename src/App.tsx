96c8c19f94d9511b981472e8d4e04c7d2b28340b
unknown <euge.ostafi@isa.utm.md>
2025-02-02 10:53:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import '../src/app.scss'
+
+function App() {
+  return (
+    <div className='hi'>Hello</div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {path: '/app', element: <App/>},
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+);
